3e863bde21251a2f6160d451a7318854da32b79d
Félix Gómez Enríquez <felixmihmo@gmail.com>
2022-10-17 10:31:55 +0200

    FGE- Calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valor1: number;
+  valor2: number;
+  resultado: number;
 
   constructor() {}
 
+  calcularSuma() {
+    this.resultado = this.valor1 + this.valor2;
+  }
+  calcularResta() {
+    this.resultado = this.valor1 - this.valor2;
+  }
+  calcularDivision() {
+    this.resultado = this.valor1 / this.valor2;
+  }
+
+  calcularMultiplicacion() {
+    this.resultado = this.valor1 * this.valor2;
+  }
+
+  reinicia() {
+    this.valor1 = null;
+    this.valor2 = null;
+    this.resultado = null;
+  }
 }
